267097efaaf27e956d660e22c0723a01c8bd45a2
ByteEntropy <34189432+ByteEntropyCom@users.noreply.github.com>
2026-04-27 18:40:19 +0800

    Update BulkheadDemoApplication.java

--- src/main/java/com/byteentropy/bulkhead_demo/BulkheadDemoApplication.java
+++ src/main/java/com/byteentropy/bulkhead_demo/BulkheadDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BulkheadDemoApplication {
+public class BulkheadCoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BulkheadDemoApplication.class, args);
+		SpringApplication.run(BulkheadCoreApplication.class, args);
 	}
 
 }
